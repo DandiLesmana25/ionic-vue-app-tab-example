602fd3b61f3797c524a5d919d7dce23dfcee4e55
DandiLesmana25 <dandilesmana.if21@gmail.com>
2026-03-06 22:42:06 +0700

    rewrite fest take photo  and display

--- src/composables/usePhotoGallery.ts
+++ src/composables/usePhotoGallery.ts
@@ -1,6 +1,8 @@
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { ref } from 'vue';
 
 export const usePhotoGallery = () => {
+     const photos = ref<UserPhoto[]>([]);
     const addNewToGallery = async () => {
     // Take a photo
     const capturedPhoto = await Camera.getPhoto({
@@ -8,9 +10,28 @@ export const usePhotoGallery = () => {
       source: CameraSource.Camera,
       quality: 100,
     });
+
+    const fileName = Date.now() + '.jpeg';
+    // CHANGE: Create `savedImageFile` matching `UserPhoto` interface
+    const savedImageFile = {
+      filepath: fileName,
+      webviewPath: capturedPhoto.webPath,
+    };
+
+    // CHANGE: Update the `photos` array with the new photo
+    photos.value = [savedImageFile, ...photos.value];
+
     };
 
     return {
-        addNewToGallery
-    }
+        addNewToGallery,
+        photos
+    };
 };
+
+
+export interface UserPhoto {
+  filepath: string;
+  webviewPath?: string;
+}
+
--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -5,17 +5,25 @@
         <ion-title>Photo Gallery</ion-title>
       </ion-toolbar>
     </ion-header>
-   <ion-content :fullscreen="true">
+    <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <!-- CHANGE: Update title -->
           <ion-title size="large">Photo Gallery</ion-title>
         </ion-toolbar>
       </ion-header>
 
-      <!-- ...existing code... -->
+      <!-- CHANGE: Add a grid component to display the photos -->
+      <ion-grid>
+        <ion-row>
+          <!-- CHANGE: Create a new column and image component for each photo -->
+          <ion-col size="6" :key="photo.filepath" v-for="photo in photos">
+            <ion-img :src="photo.webviewPath"></ion-img>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
 
-       <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+        <!-- CHANGE: Add a click event listener to the floating action button -->
         <ion-fab-button @click="addNewToGallery()">
           <ion-icon :icon="camera"></ion-icon>
         </ion-fab-button>
@@ -25,10 +33,23 @@
 </template>
 
 <script setup lang="ts">
-import { camera } from 'ionicons/icons';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonFabButton  } from '@ionic/vue';
+import { camera, trash, close } from 'ionicons/icons';
+import {
+  IonPage,
+  IonHeader,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+} from '@ionic/vue';
 
 import { usePhotoGallery } from '@/composables/usePhotoGallery';
 
-const { addNewToGallery } = usePhotoGallery();
+const { photos, addNewToGallery } = usePhotoGallery();
 </script>

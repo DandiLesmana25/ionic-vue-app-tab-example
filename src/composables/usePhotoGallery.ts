b86ca4863e4b64f92fab674ba8cf68660ec6bd0a
DandiLesmana25 <dandilesmana.if21@gmail.com>
2026-03-10 21:40:08 +0700

    saving photo to file system

--- src/composables/usePhotoGallery.ts
+++ src/composables/usePhotoGallery.ts
@@ -1,9 +1,17 @@
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { ref } from 'vue';
 
+import { Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+
+
 export const usePhotoGallery = () => {
-     const photos = ref<UserPhoto[]>([]);
-    const addNewToGallery = async () => {
+    
+  // 
+  const photos = ref<UserPhoto[]>([]);
+  
+  const addNewToGallery = async () => {
+    
     // Take a photo
     const capturedPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
@@ -11,18 +19,53 @@ export const usePhotoGallery = () => {
       quality: 100,
     });
 
+    // set filename
     const fileName = Date.now() + '.jpeg';
-    // CHANGE: Create `savedImageFile` matching `UserPhoto` interface
-    const savedImageFile = {
-      filepath: fileName,
-      webviewPath: capturedPhoto.webPath,
-    };
+    
+    //save picture and add it to photo collection 
+    const savedImageFile = await savePicture(capturedPhoto, fileName);
 
     // CHANGE: Update the `photos` array with the new photo
-    photos.value = [savedImageFile, ...photos.value];
-
+    photos.value = [savedImageFile, ...photos.value];    
     };
 
+
+
+    // save picture method
+    const savePicture = async (photo: Photo, fileName: string):Promise<UserPhoto> => {
+      // fetch the photo and read as a blob, then convert to bas64 format
+      const response = await fetch(photo.webPath!);
+      const blob = await response.blob();
+      const base64Data = (await convertBlopToBase64(blob)) as string;
+
+      const savedFile = await Filesystem.writeFile({
+        path: fileName,
+        data: base64Data,
+        directory: Directory.Data,
+      })
+ 
+      console.log(fileName);
+      console.log(photo.webPath);
+      
+      return {
+        filepath: fileName,
+        webviewPath: photo.webPath,
+      }
+    }
+    
+
+
+    const convertBlopToBase64 = (blob: Blob) => {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onerror = reject;
+        reader.onload = () => {
+          resolve(reader.result);
+        };
+        reader.readAsDataURL(blob);
+      });
+    };
+    
     return {
         addNewToGallery,
         photos
